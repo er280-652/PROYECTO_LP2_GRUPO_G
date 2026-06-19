fe4a8b7d6e0432be85caa2a980fc40bddb15b3d6
rodrigo <I202401799@CIBERTEC.EDU.PE>
2026-06-19 00:28:34 -0500

    Primer commit - Configuración de conexión a base de datos

---
+++ src/main/java/com/Finanzas/ProyectoFinanzasApplication.java
@@ -0,0 +1,13 @@
+package com.Finanzas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoFinanzasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoFinanzasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Finanzas/ProyectoFinanzasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Finanzas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoFinanzasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
