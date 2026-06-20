612127fec0076b5f284b5ce859747af8f69910f8
silva <silva@Isii>
2026-06-20 00:27:03 -0500

    Repositorios Vacios

---
+++ src/main/java/com/Finanzas/repository/MovimientoRepository.java
@@ -0,0 +1,10 @@
+package com.Finanzas.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.Finanzas.model.Tipo;
+@Repository
+public interface MovimientoRepository extends JpaRepository<MovimientoRepository, Integer>{
+
+}
