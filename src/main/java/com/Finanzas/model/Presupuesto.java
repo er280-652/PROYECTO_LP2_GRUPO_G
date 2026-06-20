3d08092467f463e5bd74189de71d1721a2f3ba55
rodrigo <I202401799@CIBERTEC.EDU.PE>
2026-06-20 01:48:20 -0500

    listado

--- src/main/java/com/Finanzas/model/Presupuesto.java
+++ src/main/java/com/Finanzas/model/Presupuesto.java
@@ -7,6 +7,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
@@ -23,6 +24,7 @@ import lombok.Setter;
 @NoArgsConstructor
 public class Presupuesto {
 
+	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "id_presupuesto")
 	private Integer idPresupuesto;
